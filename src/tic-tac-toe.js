6914c42d79d22ba0d44f8abd5e5d077f35a3a783
Alexandra-Lyskova <alexandra.lyskova@gmail.com>
2017-02-05 22:34:47 +0300

    tic-tac-toe/implementation: methods-nextTurn

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -2,7 +2,7 @@ class TicTacToe {
     constructor() {
       this.currentsym = 'x';
       this.winner = null;
-      this._isFinished = false;
+      this.isFinished = false;
       this.count = 0;
       this.field = [];
       for(var i = 0; i < 3; i++){
@@ -14,33 +14,59 @@ class TicTacToe {
     }
 
     getCurrentPlayerSymbol() {
-        return this.currentsym
+      return this.currentsym
     }
 
     nextTurn(rowIndex, columnIndex) {
-        
+      if (rowIndex > 2 || rowIndex < 0 || columnIndex > 2 || columnIndex < 0){
+        return null;
+      }else {
+
+            if(this.field[rowIndex][columnIndex] === null){
+                this.field[rowIndex][columnIndex] = this.current;
+                this.count++;
+            } else {
+                return null;
+            }
+
+      for (var i = 0; i < 3; i++){
+        if((this._field[i][0] == this.currentsym) && (this._field[i][1] == this.currentsym) && (this._field[i][2] == this.currentsym))||
+        ((this._field[0][i] == this.currentsym) && (this._field[1][i] == this.currentsym) && (this._field[2][i] == this.currentsym))||
+        ((this._field[0][0] == this.currentsym) && (this._field[1][1] == this.currentsym) && (this._field[2][2] == this.currentsym))||
+        ((this._field[2][0] == this.currentsym) && (this._field[1][1] == this.currentsym) && (this._field[0][2] == this.currentsym)){
+          this.isFinished = true;
+          this.winner = this.currentsym;
+        }
+      }
+      if(this.currentsym === 'x'){
+                this.currentsym = 'o';
+            } else {
+                this.currentsym = 'x';
+            }
+
+      }
     }
 
     isFinished() {
-        if (this.winner || this.noMoreTurns()){
+      if (this.winner || this.noMoreTurns()){
             return true;
         }
         else return false;
     }
 
     getWinner() {
-          return this.winner;
+      return this.winner;
     }
 
     noMoreTurns() {
-        if (this.count === 9){
+      if (this.count === 9){
             return true;
         }
       else return false;
     }
 
     isDraw() {
-        if((this.isFinished == true) && (this.winner == null)){
+      if((this.isFinished == true) && (this.winner == null)){
         return true;
       }else{
         return false;
